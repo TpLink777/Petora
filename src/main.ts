70ab800ee5361040fa0678c180738719ae67b722
Stiven Gómez Mazo <stivengomezmazo7@gmail.com>
2026-09-23 10:07:51 -0500

    Add
    Creacion del inicio del README

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule, ObserveInstrument } from './app.module.js';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    instrument: ObserveInstrument,
-  });
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module.js';
+import { ValidationPipe } from '@nestjs/common';
+
+async function main() {
+  const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('petora')
 }
-await bootstrap();
+
+await main();
