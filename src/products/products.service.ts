b58836773be30905b21d42d5f596b203361c6a37
Stiven Gómez Mazo <stivengomezmazo7@gmail.com>
2026-09-24 20:33:51 -0500

    Fix pagination in the endpoint
    Se creo la paginacion para el endpoint get en donde el usuario podra buscar un  producto mediante, titulo, slug y id

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,10 +2,12 @@
 import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { validate as isUUID } from 'uuid'
+
 import { CreateProductDto } from './dto/create-product.dto.js';
 import { UpdateProductDto } from './dto/update-product.dto.js';
-import { Product } from './entities/product.entity.js';
 import { PaginationDto } from '../common/dtos/pagination.dto.js';
+import { Product } from './entities/product.entity.js';
 
 @Injectable()
 export class ProductsService {
@@ -28,7 +30,7 @@ export class ProductsService {
   }
 
 
-  async findAll(paginationDto : PaginationDto) {
+  async findAll(paginationDto: PaginationDto) {
 
     const { limit = 10, offset = 0 } = paginationDto
 
@@ -40,19 +42,31 @@ export class ProductsService {
   }
 
 
-  async findOne(id: string) {
+  async findOne(term: string) {
 
-    if (!id)
-      throw new NotFoundException(`El id del producto es requerido`)
+    let product: Product | null
 
+    if (!term)
+      throw new NotFoundException(`El termino de busqueda del producto es requerido`)
 
-    const product = await this.productsRepository.findOneBy({ id })
+    if (isUUID(term)) {
+      product = await this.productsRepository.findOneBy({ id: term })
+    } else {
+
+      const queryBuilder = this.productsRepository.createQueryBuilder()
+
+      product = await
+        queryBuilder
+          .where(`UPPER(title) =:title OR slug =:slug`, {
+            title: term.toUpperCase(),
+            slug: term.toLowerCase()
+          }).getOne()
+    }
 
     if (!product)
-      throw new NotFoundException(`El producto con id ${id} no fue encontrado`)
+      throw new NotFoundException(`El producto con id ${term} no fue encontrado`)
 
     return product
-
   }
 
 
