2a1bd4caf347a9b731c5bfee956a9e7c74a85531
Stiven Gómez Mazo <stivengomezmazo7@gmail.com>
2026-09-25 21:36:22 -0500

    Add interface of seed data

---
+++ src/seed/interfaces/seed-product.interface.ts
@@ -0,0 +1,11 @@
+
+export interface SeedProduct {
+    title: string;
+    price: number;
+    description: string;
+    slug: string;
+    stock: number;
+    type: string;
+    tags: string[];
+    images: string[];
+}
